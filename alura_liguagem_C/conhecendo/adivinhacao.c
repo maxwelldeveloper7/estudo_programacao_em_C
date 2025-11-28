760f8a79b06e17e9e0aa5980ee3ddeb91482cde9
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2025-11-27 21:36:02 -0300

    aula 2

--- alura_liguagem_C/conhecendo/adivinhacao.c
+++ alura_liguagem_C/conhecendo/adivinhacao.c
@@ -12,7 +12,27 @@ int main() {
 
     int chute;
     
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
+    for(int i = 1; i <= 3; i++) {
+        printf("Tentativa %d de 3\n", i);
+
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+
+        int acertou = (chute == numeroSecreto);
+
+        if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+        } else {
+
+            int maior = (chute > numeroSecreto);
+
+            if(maior) {
+                printf("Seu chute foi maior que o número secreto!\n");
+            } else {
+                printf("Seu chute foi menor que o número secreto!\n");
+            }
+        }
+    }
+    printf("Fim de jogo!\n");
 }
